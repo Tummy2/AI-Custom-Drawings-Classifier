9c62bce3b90dfb65269d7e9b5af07b1affd5dd52
Tommy Le <144294383+Tummy2@users.noreply.github.com>
2024-10-09 00:27:53 -0500

    FINISHED
    Completed the last few functions, commented most stuff, might add more comments later on who knows. Done for know

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os.path
 
 import tkinter.messagebox
 from tkinter import *
-from tkinter import simpledialog
+from tkinter import simpledialog, filedialog
 
 import PIL
 import PIL.Image, PIL.ImageDraw
@@ -15,6 +15,7 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 
 class DrawingClassifier:
 
@@ -39,8 +40,8 @@ class DrawingClassifier:
         msg = Tk()
         msg.withdraw()
 
-        self.prob_name = simpledialog.askstring("Project Name", "Enter your project name", parent = msg)
-        if os.path.exists(self.proj_name):
+        self.proj_name = simpledialog.askstring("Project Name", "Enter your project name", parent = msg)
+        if os.path.exists(self.proj_name):          #If the project already exists load the data
             with open(f"{self.proj_name}/{self.proj_name}_data.pickle", "rb") as f:
                 data = pickle.load(f)
             self.class1 = data['c1']
@@ -51,7 +52,7 @@ class DrawingClassifier:
             self.class3_counter = data['c3c']
             self.clf = data['clf']
             self.proj_name = data['pname']
-        else:
+        else:           #If not then get the 3 new classes, initialize variables, and create directories
             self.class1 = simpledialog.askstring("Class 1", "What is the first class called?", parent = msg)
             self.class2 = simpledialog.askstring("Class 2", "What is the second class called?", parent = msg)
             self.class3 = simpledialog.askstring("Class 3", "What is the third class called?", parent = msg)
@@ -69,7 +70,7 @@ class DrawingClassifier:
             os.mkdir(self.class3)
             os.chdir("..")
 
-    def init_gui(self):
+    def init_gui(self):         #Load GUI
         WIDTH = 500
         HEIGHT = 500
         WHITE = (255, 255, 255)
@@ -91,19 +92,22 @@ class DrawingClassifier:
         btn_frame.columnconfigure(2, weight=1)
 
         class1_btn = Button(btn_frame, text=self.class1, command=lambda: self.save(1))
-        class1_btn.grid(row=0, column=0, stick=W + E)
+        class1_btn.grid(row=0, column=0, sticky=W + E)
 
-        class2_btn = Button(btn_frame, text=self.class1, command=lambda: self.save(2))
-        class2_btn.grid(row=0, column=1, stick=W + E)
+        class2_btn = Button(btn_frame, text=self.class2, command=lambda: self.save(2))
+        class2_btn.grid(row=0, column=1, sticky=W + E)
 
-        class3_btn = Button(btn_frame, text=self.class1, command=lambda: self.save(3))
-        class3_btn.grid(row=0, column=2, stick=W + E)
+        class3_btn = Button(btn_frame, text=self.class3, command=lambda: self.save(3))
+        class3_btn.grid(row=0, column=2, sticky=W + E)
 
         bm_btn = Button(btn_frame, text="Brush-", command=self.brushminus)
         bm_btn.grid(row=1, column=0, sticky=W + E)
 
         clear_btn = Button(btn_frame, text="Clear", command=self.clear)
         clear_btn.grid(row=1, column=1, sticky=W + E)
+
+        bp_btn = Button(btn_frame, text="Brush+", command=self.brushplus)
+        bp_btn.grid(row=1, column=2, sticky=W + E)
 
         train_btn = Button(btn_frame, text="Train Model", command=self.train_model)
         train_btn.grid(row=2, column=0, sticky=W + E)
@@ -123,46 +127,138 @@ class DrawingClassifier:
         save_everything_btn = Button(btn_frame, text="Save Everything", command=self.save_everything)
         save_everything_btn.grid(row=3, column=2, sticky=W + E)
 
-        self.status_label = Label(btn_frame, text=f"Current Moddel: {type(self.clf).__name__}")
+        self.status_label = Label(btn_frame, text=f"Current Model: {type(self.clf).__name__}")
         self.status_label.config(font=("Arial", 10))
         self.status_label.grid(row=4, column=1, sticky=W + E)
 
-        self.root.protocol("WM_DDELETED_WINDOW", self.on_closing)
+        self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
         self.root.attributes("-topmost", True)
         self.root.mainloop()
 
     def paint(self, event):
-        pass
+        x1, y1 = (event.x - 1), (event.y - 1)
+        x2, y2 = (event.x + 1), (event.y + 1)
+        self.canvas.create_rectangle(x1, y1, x2, y2, fill="black", width=self.brush_width)
+        self.draw.rectangle([x1, y1, x2 + self.brush_width, y2 + self.brush_width], fill="black", width=self.brush_width)
 
-    def save(self, class_num):
-        pass
+    def save(self, class_num):          #Saves an image as temp.png, if the image is classified under class x, create or save to that directory
+        self.image1.save("temp.png")
+        img = PIL.Image.open("temp.png")
+        img.thumbnail((50, 50), PIL.Image.Resampling.LANCZOS)
+
+        if class_num == 1:
+            img.save(f"{self.proj_name}/{self.class1}/{self.class1_counter}.png", "PNG")
+            self.class1_counter += 1
+        elif class_num == 2:
+            img.save(f"{self.proj_name}/{self.class2}/{self.class2_counter}.png", "PNG")
+            self.class2_counter += 1
+        elif class_num == 3:
+            img.save(f"{self.proj_name}/{self.class3}/{self.class3_counter}.png", "PNG")
+            self.class3_counter += 1
+
+        self.clear()
 
     def brushminus(self):
-        pass
+        if self.brush_width > 1:
+            self.brush_width -= 1
 
     def clear(self):
-        pass
+        self.canvas.delete("all")
+        self.draw.rectangle([0, 0, 1000, 1000], fill="white")
 
     def brushplus(self):
-        pass
+        self.brush_width += 1
 
     def train_model(self):
-        pass
+        img_list = np.array([])         #Numpy array full of pixels
+        class_list = np.array([])        #Numpy array full of labels (classes) that go with image
+
+        for x in range(1, self.class1_counter):
+            img = cv.imread(f"{self.proj_name}/{self.class1}/{x}.png")[:, :, 0]         #Load the image
+            img = img.reshape(2500)         #Flatten array into a 1D vector that is 2500 long
+            img_list = np.append(img_list, [img])           #Add image into img list
+            class_list = np.append(class_list, 1)           #Add the corresponding class of the img to the class list
+
+        for x in range(1, self.class2_counter):
+            img = cv.imread(f"{self.proj_name}/{self.class2}/{x}.png")[:, :, 0]         #Load the image
+            img = img.reshape(2500)         #Flatten array into a 1D vector that is 2500 long
+            img_list = np.append(img_list, [img])           #Add image into img list
+            class_list = np.append(class_list, 2)           #Add the corresponding class of the img to the class list
+
+        for x in range(1, self.class3_counter):
+            img = cv.imread(f"{self.proj_name}/{self.class3}/{x}.png")[:, :, 0]         #Load the image
+            img = img.reshape(2500)         #Flatten array into a 1D vector that is 2500 long
+            img_list = np.append(img_list, [img])           #Add image into img list
+            class_list = np.append(class_list, 3)           #Add the corresponding class of the img to the class list
+
+        img_list = img_list.reshape(self.class1_counter - 1 + self.class2_counter - 1 + self.class3_counter - 1, 2500)
+
+        self.clf.fit(img_list, class_list)
+        tkinter.messagebox.showinfo("Drawing Classifier", "Model successfully trained", parent = self.root)
 
     def predict(self):
-        pass
+        self.image1.save("temp.png")
+        img = PIL.Image.open("temp.png")
+        img.thumbnail((50,50), PIL.Image.Resampling.LANCZOS)
+        img.save("predictshit.png", "PNG")
 
-    def rotate_model(self):
-        pass
+        img = cv.imread("predictshit.png")[:, :, 0]
+        img = img.reshape(2500)
+        prediction = self.clf.predict([img])
+        if prediction[0] == 1:
+            tkinter.messagebox.showinfo("Drawing Classifier", f"Probably a {self.class1}", parent=self.root)
+        elif prediction[0] == 2:
+            tkinter.messagebox.showinfo("Drawing Classifier", f"Probably a {self.class2}", parent=self.root)
+        elif prediction[0] == 3:
+            tkinter.messagebox.showinfo("Drawing Classifier", f"Probably a {self.class3}", parent=self.root)
+
+    def rotate_model(self):         #Switch between models
+        if isinstance(self.clf, LinearSVC):
+            self.clf = KNeighborsClassifier()
+        elif isinstance(self.clf, KNeighborsClassifier):
+            self.clf = LogisticRegression()
+        elif isinstance(self.clf, LogisticRegression):
+            self.clf = DecisionTreeClassifier()
+        elif isinstance(self.clf, DecisionTreeClassifier):
+            self.clf = RandomForestClassifier()
+        elif isinstance(self.clf, RandomForestClassifier):
+            self.clf = GaussianNB()
+        elif isinstance(self.clf, GaussianNB):
+            self.clf = LinearSVC()
+
+        self.status_label.config(text=f"Current Model: {type(self.clf).__name__}")
 
     def save_model(self):
-        pass
+        file_path = filedialog.asksaveasfilename(defaultextension="pickle")
+        with open(file_path, "wb") as f:
+            pickle.dump(self.clf, f)
+        tkinter.messagebox.showinfo("Drawing Classifier", "Model sucessfully saved", parent=self.root)
 
     def load_model(self):
-        pass
+        file_path = filedialog.askopenfilename()
+        with open(file_path, "rb") as f:
+            self.clf = pickle.load(f)
+        tkinter.messagebox.showinfo("Drawing Classifier", "Model sucessfully loaded", parent=self.root)
 
     def save_everything(self):
-        pass
+        data = {"c1": self.class1, "c2": self.class2, "c3": self.class3, 
+                "c1c": self.class1_counter, "c2c": self.class2_counter, "c3c": self.class3_counter, 
+                "clf": self.clf, "pname": self.proj_name}
+        with open(f"{self.proj_name}/{self.proj_name}_data.pickle", "wb") as f:
+            pickle.dump(data, f)
+        tkinter.messagebox.showinfo("Drawing Classifier", "Project successully saved", parent=self.root)
 
     def on_closing(self):
-        pass
+        answer = tkinter.messagebox.askyesnocancel("Quit?", "Do you want to save?", parent=self.root)
+        if answer is not None:
+            if answer:
+                self.save_everything()
+            self.root.destroy()
+            exit()   
+
+def main():
+    DrawingClassifier()
+
+if __name__ == "__main__":
+    main()
+
